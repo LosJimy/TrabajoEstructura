1881cb203ce06a83c1485be74f4ac6a769d82608
LosJimy <112668599+LosJimy@users.noreply.github.com>
2025-01-21 20:58:19 +0000

    a

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
-#include <queue>
+#include <sstream>
 
 using namespace std;
 
@@ -12,22 +12,39 @@ int main(){
     cin >> nombreArch;
     fstream arch(nombreArch);
 
-    string linea;
-    if(arch.is_open()){
+    if (arch.is_open()){
         int n;
-        arch >> n;
+        arch >>  n;
         arch.ignore();
+        
+        vector<vector<int>> matriz(n, vector<int>(n));
 
-        for(int i=0; i<n;i++){
-            for(int j =0; j<n;j++){
-                arch >> linea;
-                cout << linea << endl;
+        for(int i =0; i<n; i++){
+            string linea;
+            getline(arch, linea);
+            stringstream ss(linea);
+            string num;
+
+            for(int j= 0;j<n;j++){
+                getline(ss, num, ',');
+                matriz[i][j] = stoi(num);
             }
         }
+
         arch.close();
+
+        cout << "Matriz leida del archivo: " << endl;
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout << matriz[i][j] << " ";
+            }
+            cout << endl;
+        }
+
     }else{
-        cout << "No se pudo abrir el archivo";
+        cout << "No se pudo abrir el archivo. " << endl;
     }
+
 
     return 0;
 }
